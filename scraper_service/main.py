f9b0ebcebd1d7e8c2c4274f1fc261c6e4ba28637
easontseng <yhsints.dev.gmail.com>
2024-08-11 01:58:54 +0800

    [Feat]添加定時爬蟲機制

--- scraper_service/main.py
+++ scraper_service/main.py
@@ -1,9 +1,23 @@
 from controllers.jobs import JobController
+import schedule
+import time
 
-def main():
+# def main():
+#     keyword = 'node.js'  # 修改為你需要的關鍵字
+#     job_controller = JobController()  # 創建 JobController 實例
+#     job_controller.search_and_save_jobs(keyword)  # 使用 JobController 進行操作
+
+def job():
     keyword = 'node.js'  # 修改為你需要的關鍵字
     job_controller = JobController()  # 創建 JobController 實例
     job_controller.search_and_save_jobs(keyword)  # 使用 JobController 進行操作
+
+def main():
+    schedule.every().day.at("09:00").do(job)  # 每天上午9點執行
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
